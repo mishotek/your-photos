75d13e60dda6a302d0e7f6612512affe3e3c9c01
mishotek <mtekt16@freeuni.edu.ge>
2020-10-07 12:47:14 +0400

    # eslint added

--- server/index.js
+++ server/index.js
@@ -8,11 +8,10 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const BASE_PATH =  path.resolve(__dirname, '..');
+const BASE_PATH = path.resolve(__dirname, '..');
 
 app.use('/', express.static(`${BASE_PATH}/public`));
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
-
